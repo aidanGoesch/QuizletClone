04a637b7d74da82ee933bb4ff7ddd0e146d20bf9
Aidan Goeschel <agoesche@uci.edu>
2023-09-05 14:56:11 -0700

    changed path for logging

--- source/Backend/Log/logging.py
+++ source/Backend/Log/logging.py
@@ -1,13 +1,13 @@
 import pathlib
 
 def reset_log() -> None:
-    file_path = pathlib.Path("./Backend/Log/log.txt")
+    file_path = pathlib.Path("source/Backend/Log/log.txt")
     with open(file_path, "w") as file:
         file.write("### EVENT LOG ###")
 
 
 def log(text: str) -> None:
-    file_path = pathlib.Path("./Backend/Log/log.txt")
+    file_path = pathlib.Path("source/Backend/Log/log.txt")
     with file_path.open(mode = "a") as file:
         file.write(f"\n{text}")
 
